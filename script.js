5fab94e20064ae287ff969f006215cb1ae330786
Julian Vrtiška <julv1301@gmail.com>
2026-02-19 19:34:38 +0100

    preparation for fetching data from api

--- script.js
+++ script.js
@@ -6,5 +6,15 @@ document.addEventListener("scroll", () => {
     
     const pageReadenPercent = (scrollTop / (scrollHeight - clientHeight)) * 100;
     document.getElementById("read-progress-bar").style.width = pageReadenPercent + "%";
-     
+
 });
+
+const getDriverInfo = async () => {
+    try {
+        const resp = await fetch("https://api.openf1.org/v1/drivers?driver_number=1&session_key=9158");
+        const data = await resp.json();
+        console.log(data);
+    } catch (error) {
+        console.log("error: ", error);
+    }
+}
