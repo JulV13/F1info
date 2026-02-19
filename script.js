e7d54c0570b5a8aff8ddc3248ad4f96d1048077a
Julian Vrtiška <julv1301@gmail.com>
2026-02-19 18:33:13 +0100

    page read progress bar

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("scroll", () => {
+
+    const scrollTop = document.documentElement.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight;
+    const clientHeight = document.documentElement.clientHeight;
+    
+    const pageReadenPercent = (scrollTop / (scrollHeight - clientHeight)) * 100;
+    document.getElementById("read-progress-bar").style.width = pageReadenPercent + "%";
+     
+});
