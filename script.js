ebda88a39b1a1f7f042b701ee139f868bd3e16f6
Julian Vrtiška <julv1301@gmail.com>
2026-02-25 18:12:06 +0100

    added points for drivers in WDC

--- script.js
+++ script.js
@@ -11,18 +11,25 @@ document.addEventListener("scroll", () => {
 
 const getDriversChampionshipInfo = async () => {
     try {
-        const driverChampionshipResponse = await fetch("https://api.openf1.org/v1/drivers?session_key=latest");
-        const driverChampionshipData = await driverChampionshipResponse.json();
-        console.log(driverChampionshipData);
+        const driversResponse = await fetch("https://api.openf1.org/v1/drivers?session_key=9839");
+        const driverData = await driversResponse.json();
+        console.log(driverData);
+        const driversChampionshipResponse = await fetch("https://api.openf1.org/v1/championship_drivers?session_key=9839");
+        const driversChampionshipData = await driversChampionshipResponse.json();
+        console.log(driversChampionshipData );
+
         var driverChampionshipBox = document.getElementById("driverChampionshipBox");
         driverChampionshipBox.innerHTML = '';
         driverChampionshipBox.style.visibility="visible";
-        driverChampionshipData.forEach(driver => {
+
+        driverData.forEach(driver => {
+            const stats = driversChampionshipData.find(stat => stat.driver_number === driver.driver_number);
             driverChampionshipBox.innerHTML +=`
                 <div class="driverCard" style="background-color: #${driver.team_colour}">
                     <img src="${driver.headshot_url}" class="driverImage" style="border: 4px solid #${driver.team_colour}">
                     <span>${driver.first_name} ${driver.last_name}</span>
                     <span class="driverNumber">${driver.driver_number}</span>
+                    <span class="driverNumber">points:${stats ? stats.points_current : 0}</span>
                     <span>${driver.team_name}</span>
                 </div>
             `;    
