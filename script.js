6f185b18ea1e55fcec9d4f187e2aa0ee52110fc6
Julian Vrtiška <julv1301@gmail.com>
2026-02-25 14:58:13 +0100

    driver championship data fetching from API

--- script.js
+++ script.js
@@ -9,7 +9,34 @@ document.addEventListener("scroll", () => {
 
 });
 
-const getDriverInfo = async () => {
+//<div class="teamColorBox" style="backgroundColor: #${driver.team_colour}"></div>
+
+const getDriversChampionshipInfo = async () => {
+    try {
+        const response = await fetch("https://api.openf1.org/v1/drivers?session_key=latest");
+        const data = await response.json();
+        console.log(data);
+        var driverChampionshipBox = document.getElementById("driverChampionshipBox");
+        driverChampionshipBox.innerHTML = '';
+        driverChampionshipBox.style.visibility="visible";
+        data.forEach(driver => {
+            driverChampionshipBox.innerHTML +=`
+                <div class="driverCard">
+                    <img src="${driver.headshot_url}" class="driverImage" style="border: 4px solid #${driver.team_colour}">
+                    ${driver.first_name} 
+                    ${driver.last_name}
+                    ${driver.driver_number}
+                    ${driver.team_name}
+                </div>
+            `;    
+        });
+    } catch (error) {
+        console.log("error: ", error);
+        driverChampionshipBox.innerHTML = 'error occured: ' + error;
+    }
+}
+
+const getConstructorsChampionshipInfo = async () => {
     try {
         const resp = await fetch("https://api.openf1.org/v1/drivers?driver_number=1&session_key=9158");
         const data = await resp.json();
