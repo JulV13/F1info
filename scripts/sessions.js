77f6ed58d5c7d16c399d63c1c3e10571c6a6583d
Julian Vrtiška <julv1301@gmail.com>
2026-03-03 16:45:43 +0100

    sessions update including quali and race improvements

--- scripts/sessions.js
+++ scripts/sessions.js
@@ -56,11 +56,20 @@ const sessionResults = async (session_key) => {
         const sessionResultsData = await sessionResultsResponse.json();
         console.log(sessionResultsData);
 
+        const sessionResponse = await fetch(`https://api.openf1.org/v1/sessions?session_key=${session_key}`);
+        const sessionData = await sessionResponse.json();
+
         const driversResponse = await fetch(`https://api.openf1.org/v1/drivers?session_key=${session_key}`);
         const driversData = await driversResponse.json();
 
+        const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+        await sleep(1000);
+
         const driversChampionshipResponse = await fetch(`https://api.openf1.org/v1/championship_drivers?session_key=${session_key}`);
         const driversChampionshipData = await driversChampionshipResponse.json();
+
+        const startingGridResponse = await fetch(`https://api.openf1.org/v1/starting_grid?session_key=${session_key}`);
+        const startingGridData = await startingGridResponse.json();
 
         const chosenSessionInfoBox = document.getElementById(`results_${session_key}`);
         chosenSessionInfoBox.innerHTML = '';
@@ -69,50 +78,151 @@ const sessionResults = async (session_key) => {
         const hideResultsButton = document.getElementById("hideResultsButton");
         hideResultsButton.disabled = false;
 
-        const target = document.getElementById(`results_${session_key}`);
-        sessionResultsButton.addEventListener("click", () => {
-            target.scrollIntoView({behavior: 'smooth'});
-        });
+        // const target = document.getElementById(`results_${session_key}`);
+        // sessionResultsButton.addEventListener("click", () => {
+        //     target.scrollIntoView({behavior: 'smooth'});
+        // });
+    
+        let chosenSessionResultsTable = ``;
 
-        let chosenSessionResultsTable = `
-        <div style="overflow-x:auto">
-        <table class="sessionResults">
-            <thead>
-            <tr>
-                <td>POS.</td>
-                <td>DRIVER</td>
-                <td>#</td>
-                <td>PTS [<]</td>
-                <td>PTS [>]</td>
-                <td>WDC [<]</td>
-                <td>WDC [>]</td>
-                <td>LAPS</td>
-                <td>DNF/DSQ/DNS</td>
-            </tr>
-            </thead>
-            <tbody>
-        `;
-        sessionResultsData.forEach(driver => { 
+        if(`${sessionData[0].session_type}`=="Race") {
 
-            let driverInfo = driversData.find(d => d.driver_number == driver.driver_number);
-            let championshipInfo = driversChampionshipData.find(c => c.driver_number == driver.driver_number);
-            let lastName = `${driverInfo.last_name}`;
-            let lastNameSliced = lastName.slice(0,3).toUpperCase();
-
-            chosenSessionResultsTable+=`
+            chosenSessionResultsTable = `
+            <div style="overflow-x:auto">
+            <table class="sessionResults">
+                <thead>
                 <tr>
-                    <td>${driver.position ?? "-"}</td>
-                    <td>${lastNameSliced}</td>
-                    <td>${driver.driver_number}</td>
-                    <td>${championshipInfo.points_start}</td>
-                    <td>${championshipInfo.points_current}</td>
-                    <td>${championshipInfo.position_start ?? "-"}</td>
-                    <td>${championshipInfo.position_current}</td>
-                    <td>${driver.number_of_laps}</td>
-                    <td>${driver.dnf === false ? "❌" : "✅"}/${driver.dsq === false ? "❌" : "✅"}/${driver.dns === false ? "❌" : "✅"}</td>
+                    <td>POS.</td>
+                    <td>DRIVER</td>
+                    <td>POINTS</td>
+                    <td>WDC</td>
+                    <td>LAPS</td>
+                    <td>TIME</td>
+                    <td>DNF/DSQ/DNS</td>
                 </tr>
+                </thead>
+                <tbody>
             `;
-        });
+
+            sessionResultsData.forEach(driver => { 
+                    let driverInfo = driversData.find(d => d.driver_number == driver.driver_number);
+                    let championshipInfo = driversChampionshipData.find(c => c.driver_number == driver.driver_number);
+                    let lastName = `${driverInfo.last_name}`;
+                    let lastNameSliced = lastName.slice(0,3).toUpperCase();
+
+                    let raceHours = Math.trunc(driver.duration / 3600);
+                    let raceMinutes = Math.trunc((driver.duration / 60) - (raceHours*60));
+                    let raceSeconds = Math.trunc((driver.duration) - (raceHours*3600) - (raceMinutes*60));
+
+                    if (raceSeconds / 10 < 1){
+                        raceSeconds = `0${raceSeconds}`;
+                    }
+
+                    if (raceHours / 10 < 1){
+                        raceHours = `0${raceHours}`;
+                    }
+
+                    let raceTime = `${raceHours}:${raceMinutes}:${raceSeconds}`;
+                    if (raceTime==`0:0:00`) {
+                        raceTime='-';
+                    }
+
+                    chosenSessionResultsTable+=`
+                        <tr>
+                            <td>${driver.position ?? "-"}</td>
+                            <td>${lastNameSliced} #${driver.driver_number}</td>
+                            <td>${championshipInfo.points_start} -> ${championshipInfo.points_current}</td>
+                            <td>${championshipInfo.position_start ?? "0"} -> ${championshipInfo.position_current}</td>
+                            <td>${driver.number_of_laps}</td>
+                            <td>${raceTime}</td>
+                            <td>${driver.dnf === false ? "❌" : "✅"}/${driver.dsq === false ? "❌" : "✅"}/${driver.dns === false ? "❌" : "✅"}</td>
+                        </tr>
+                    `;
+            });
+        }
+
+        if(`${sessionData[0].session_type}`=="Qualifying"){
+
+            chosenSessionResultsTable = `
+            <div style="overflow-x:auto">
+            <table class="sessionResults">
+                <thead>
+                <tr>
+                    <td>POS.</td>
+                    <td>DRIVER</td>
+                    <td>LAPS</td>
+                    <td>Q1</td>
+                    <td>Q2</td>
+                    <td>Q3</td>
+                    <td>DNF/DSQ/DNS</td>
+                </tr>
+                </thead>
+                <tbody>
+            `;        
+            
+            sessionResultsData.forEach(driver => { 
+
+                    let driverInfo = driversData.find(d => d.driver_number == driver.driver_number);
+                    let sessionResultsInfo = sessionResultsData.find(ss => ss.driver_number == driver.driver_number)
+                    let lastName = `${driverInfo.last_name}`;
+                    let lastNameSliced = lastName.slice(0,3).toUpperCase();
+
+                    let q1time = driver.duration[0];
+                    let q2time = driver.duration[1];
+                    let q3time = driver.duration[2];
+
+                    let q1Minutes = Math.trunc((q1time / 60));
+                    let q1Seconds = Math.trunc((q1time) - (q1Minutes*60));
+                    let q1miliSeconds = Math.trunc((q1time % 1) * 1000);
+
+                    let q2Minutes = Math.trunc((q2time / 60));
+                    let q2Seconds = Math.trunc((q2time) - (q2Minutes*60));
+                    let q2miliSeconds = Math.trunc((q2time % 1) * 1000);
+
+                    let q3Minutes = Math.trunc((q3time / 60));
+                    let q3Seconds = Math.trunc((q3time) - (q3Minutes*60));
+                    let q3miliSeconds = Math.trunc((q3time % 1) * 1000);
+
+                    if (q1Seconds / 10 < 1){
+                        q1Seconds = `0${q1Seconds}`;
+                    }
+
+                    if (q2Seconds / 10 < 1){
+                        q2Seconds = `0${q2Seconds}`;
+                    }
+
+                    if (q3Seconds / 10 < 1){
+                        q3Seconds = `0${q3Seconds}`;
+                    }
+
+                    let q1fullTime = `${q1Minutes}:${q1Seconds}:${q1miliSeconds}`;
+                    if (q1fullTime == '0:00:0') {
+                        q1fullTime = '-'
+                    }
+
+                    let q2fullTime = `${q2Minutes}:${q2Seconds}:${q2miliSeconds}`;
+                    if (q2fullTime == '0:00:0') {
+                        q2fullTime = '-'
+                    }
+                    
+                    let q3fullTime = `${q3Minutes}:${q3Seconds}:${q3miliSeconds}`;
+                    if (q3fullTime == '0:00:0') {
+                        q3fullTime = '-'
+                    }
+
+                    chosenSessionResultsTable+=`
+                        <tr>
+                            <td>${driver.position ?? "-"}</td>
+                            <td>${lastNameSliced} #${driver.driver_number}</td>
+                            <td>${sessionResultsInfo.number_of_laps ?? "-"}</td>
+                            <td>${q1fullTime}</td>
+                            <td>${q2fullTime}</td>
+                            <td>${q3fullTime}</td>
+                            <td>${sessionResultsInfo.dnf === false ? "❌" : "✅"}/${sessionResultsInfo.dsq === false ? "❌" : "✅"}/${sessionResultsInfo.dns === false ? "❌" : "✅"}</td>
+                        </tr>
+                    `;                
+            });
+        }
         chosenSessionResultsTable+=`
             </tbody>
         </table>
